1e4db144037ee504426c3d97a24d18497389ee99
KryptomSilver <abelromeror763@gmail.com>
2021-05-24 13:45:49 -0500

    Server terminado

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// habilitar cors
+app.use(cors());
+//Morgan para poder ver petciones
+app.use(morgan("dev"));
+// Habilitar express.json
+app.use( express.json({ extended: true }));
+
+// puerto de la app
+const port = process.env.PORT || 4000;
+
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas'));
+
+// arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
